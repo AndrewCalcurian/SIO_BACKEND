039ae439b9404aa6c886300ececd79aa6f0be731
Andres casa <calcurianandres@gmail.com>
2023-10-02 23:26:13 -0400

    2102023

--- server/routes/asignacion.routes.js
+++ server/routes/asignacion.routes.js
@@ -3,12 +3,15 @@ const app = express();
 
 const Orden = require('../database/models/orden.model');
 const Almacenado = require('../database/models/almacenado.model');
+const Iasignacion = require('../database/models/iasignacion.modal')
+const Lotes = require('../database/models/lotes.model')
+const Requisicion = require('../database/models/requisicion.model')
 
 const {FAL005} = require('../middlewares/docs/FAL-005-test.pdf')
 
 app.get('/api/orden-especifica', (req, res)=>{
 
-    Orden.find({sort:'2023061'})
+    Orden.find({estado:'Espera'})
         .populate('cliente')
         .populate('producto.grupo')
         .populate('producto.cliente')
@@ -120,9 +123,94 @@ app.get('/api/buscar-cinta', (req, res)=>{
 app.post('/api/descontar', (req, res)=>{
     let body = req.body;
 
-    FAL005('2023061', '0000', body.tabla, body.materiales, body.lotes, body.cantidades)
+    let N_asignacion;
+    let lotes = [];
+    let Requi = false;
 
-    res.json('done')
+
+    if(body.requi){
+        Requi = true
+        Requisicion.findOneAndUpdate({_id:body.orden_id},{estado:'Finalizado'}, (err, requi)=>{
+            if( err ){
+                return res.status(400).json({
+                    ok:false,
+                    err
+                });
+            }
+        })
+
+    }
+
+
+
+    Orden.findOneAndUpdate({sort:body.orden}, {estado:'activo'}, (err, modificado)=>{
+        if( err ){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+    }
+    })
+
+
+    Iasignacion.findByIdAndUpdate({_id: 'iterator'}, {$inc: {seq: 1}}, {new: true, upset:true}, (err, asig)=>{
+        if( err ){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
+        N_asignacion = asig.seq
+    })
+    
+    for(let i=0;i<body.ids.length;i++){
+
+        Almacenado.updateOne({_id:body.ids[i]}, {cantidad:body.restantes[i]}, (err, almacenadoDB) =>{
+            if( err ){
+                return res.status(400).json({
+                    ok:false,
+                    err
+                });
+            }
+
+            let lote = {
+                asignacion:N_asignacion,
+                material:body.producto[i],
+                lote:body.lotes[i],
+                codigo:body.codigos[i],
+                cantidad:body.cantidad[i],
+                EA_cantidad:body.EA_cantidad[i]
+            }
+
+            lotes.push(lote)
+
+            console.log(`lote:${body.lotes[i]} codigo:${body.codigos[i]} - EA:${body.EA_cantidad[i]} updated to ${body.restantes[i]}`)
+            
+            if(i == body.ids.length - 1){
+                console.log('asignacion: ',N_asignacion)
+                let NewLote = {
+                    asignacion:N_asignacion,
+                    orden:body.orden,
+                    material:lotes
+                }
+                let lotes_ = new Lotes(NewLote).save((err, loteDB)=>{
+                    if( err ){
+                        return res.status(400).json({
+                            ok:false,
+                            err
+                        });
+                    }
+
+                    console.log(`se registro nuevo lote: ${loteDB}`)
+                    // FAL005(body.orden, N_asignacion, body.tabla, body.materiales, body.lotes, body.cantidades,Requi)
+                    res.json('done')
+                })
+            }
+        })
+    
+    }
+
+
 })
 
 
--- server/server.js
+++ server/server.js
@@ -3,18 +3,8 @@ require('./config/.env');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const Counter = require('../server/database/models/orden.model');
-const convCount = require('../server/database/models/conversiones.model');
 
 
-const Usuario = require('../server/database/models/usuarios.model')
-const bcrypt = require('bcrypt');
-
-const {FAL005} = require('./middlewares/docs/FAL-005.pdf')
-
-const isolicitud = require('./database/models/isolicitud.modal')
-const iasignacion = require('./database/models/iasignacion.modal')
-const idevolucion = require('./database/models/idevolucion.model')
 
 //server
 const app = express();
@@ -36,35 +26,6 @@ app.use ('**', (req,res)=>{
 
 //Base de datos
 require('./database/connection');
-
-// SolicitudMateria('002022','test')
-
-//  let usuario = new Usuario({
-//     Role: "Desarrollador",
-//     estado: true,
-//     Departamento: "Desarrollo",
-//     Nueva_orden: 0,
-//     Consulta: 1,
-//     Almacen: 0,
-//     Maquinaria: 0,
-//     Planificacion: 1,
-//     Gestiones: 0,
-//     Nombre: "Preprensa",
-//     Apellido: "Preprensa",
-//     Correo: "preprensa@poligraficaindustrial.com",
-//     Password: bcrypt.hashSync('1234567', 10),
-
-//   }).save();
-
-//    let contador = new idevolucion({
-//        _id:'test',
-//       seq:21000
-//    }).save();
-
-//  const materiales = ['test','test','test']
-//  const lotes = ['test','test','test'];
-//  const cantidades = ['test','test','test']
-//  FAL005(001,001, materiales, lotes, cantidades);
 
 //correr app
 app.listen(process.env.PORT, ()=>{
